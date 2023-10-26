b8c6ed6a48d53830de64112b643f574d29c2f392
arefin_shawon <arefin.shawon07@gmail.com>
2023-10-26 14:19:32 +0600

    Customize Odoo's Web Header

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Customize Title Header',
+    'version': '16.0.1.0.0',
+    'sequence': 0,
+    'summary': "Choose your own Title to display on the browser header.",
+    'author': 'arefin_shawon',
+    # 'maintainer': 'Hasinur Arefin Shawon',
+    # 'price': '0.0',
+    # 'currency': 'USD',
+    # 'website': '',
+    'category': 'Themes',
+    'license': 'LGPL-3',
+    'depends': ['web'],
+    'data': [
+        
+    ],
+    'assets': {
+        'web.assets_backend_prod_only': [
+            'customize_title_header/static/src/js/favicon.js',
+        ],
+    },
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+        
+    ],
+}
